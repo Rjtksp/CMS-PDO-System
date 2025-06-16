ee22ad6b4958ca87a41ac5a5fa5cc9982d6b4465
Rajat Kashyap <35030513+Rjtksp@users.noreply.github.com>
2025-06-16 19:34:29 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,59 @@
+<?php 
+include "partials/header.php"; 
+include "partials/navbar.php"; 
+include "partials/hero.php"; 
+?>
+
+<!-- Main Content -->
+<main class="container my-5">
+    <h2>Our Story</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor arcu quis nibh dapibus, id mollis leo dignissim. 
+        Donec euismod, nisl eget consectetur sagittis, nisl nunc ultricies nisi, in semper nisl nunc eget lacus.
+    </p>
+    <p>
+        Aliquam erat volutpat. Praesent at lacus in sem accumsan suscipit. Integer a odio at nisi aliquam vulputate.
+    </p>
+    <h2>Our Team</h2>
+    <p>
+        Meet the passionate individuals behind our blog.
+    </p>
+    <!-- Team Members -->
+    <div class="row">
+        <!-- Team Member 1 -->
+        <div class="col-md-4 text-center">
+            <img
+                src="https://templates-flatlogic.herokuapp.com/sing-app/html5/demo/img/people/a5.jpg"
+                class="rounded-circle mb-3"
+                alt="Team Member"
+                height="300"
+            >
+            <h5>Rajat Kashyap</h5>
+            <p class="text-muted">Founder & Editor</p>
+        </div>
+        <!-- Team Member 2 -->
+        <div class="col-md-4 text-center">
+            <img
+                src="https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg"
+                class="rounded-circle mb-3"
+                alt="Team Member"
+                height="300"
+            >
+            <h5>Rose Diaz</h5>
+            <p class="text-muted">Content Writer</p>
+        </div>
+        <!-- Team Member 3 -->
+        <div class="col-md-4 text-center">
+            <img
+                src="https://www.electricallicenserenewal.com/app-assets/images/user/12.jpg"
+                class="rounded-circle mb-3"
+                alt="Team Member"
+                height="300"
+            >
+            <h5>Maria Gonzalez</h5>
+            <p class="text-muted">Graphic Designer</p>
+        </div>
+    </div>
+</main>
+
+<?php include "partials/footer.php"; ?>
