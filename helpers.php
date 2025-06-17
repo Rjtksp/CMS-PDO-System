d1ba9f1d4bb5584a1e10bb4e0f88aa952aa5f94b
Rajat Kashyap <35030513+Rjtksp@users.noreply.github.com>
2025-06-17 19:01:59 +0530

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -3,12 +3,12 @@
 function base_url($path = "") {
     $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== "off" ? 'https://' : 'http://';
     $host = $_SERVER['HTTP_HOST'];
-    $baseUrl = $protocol . $host;
+    $baseUrl = $protocol . $host . '/' . PROJECT_DIR;
     return $baseUrl . '/' . ltrim($path, '/');
 }
 
 function base_path($path = "") {
-    $rootPath = dirname(__DIR__);
+    $rootPath = dirname(__DIR__) . DIRECTORY_SEPARATOR . PROJECT_DIR;
     return $rootPath . DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR);
 }
 
@@ -27,4 +27,12 @@ function asset_url($path = "") {
 function redirect($url) {
     header('Location: ' . base_url($url));
     exit;
+}
+
+function isPostRequest() {
+    return $_SERVER['REQUEST_METHOD'] === 'POST';
+}
+
+function getPostData($fields, $default = null) {
+    return isset($_POST[$fields]) ? trim($_POST[$fields]) : $default;
 }
