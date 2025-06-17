caea34931801ca6dc14c33ff91160af35b01c7dd
Rajat Kashyap <35030513+Rjtksp@users.noreply.github.com>
2025-06-17 19:03:18 +0530

    Update init.php

--- init.php
+++ init.php
@@ -1,6 +1,22 @@
 <?php
+
+// include autoloader
+require_once "autoloader.php";
+
+// start session
 session_start();
+
+// include the main configuration file
+require_once "config/config.php";
+
+// load database
+// require_once "classes/Database.php";
+
+// include helper functions
 require_once "helpers.php";
+
+// define global constant
 define("APP_NAME", "CMS PDO System");
+define("PROJECT_DIR", "cms-pdo");
 
 ?>
