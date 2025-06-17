84307fc9b486a0e13ced66de6abca4a3dc90ecab
Rajat Kashyap <35030513+Rjtksp@users.noreply.github.com>
2025-06-17 19:02:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,69 +1,44 @@
 <?php 
-include "partials/header.php"; 
-include "partials/navbar.php"; 
-include "partials/hero.php"; 
+require_once "partials/header.php"; 
+include base_path("partials/navbar.php"); 
+include base_path("partials/hero.php"); 
+
+$article = new Article();
+$articles = $article->getArticles();
+
 ?>
 
 <!-- Main Content -->
 <main class="container my-5">
+    <?php if(!empty($articles)): ?>
+        <?php foreach($articles as $article): ?>
 
-    <!-- Blog Post 1 -->
-    <div class="row mb-4">
-        <div class="col-md-4">
-            <img
-                src="https://dummyjson.com/image/350x200/333333/eae0d0"
-                class="img-fluid"
-                alt="Blog Post Image"
-            >
+        <!-- Articles -->
+        <div class="row mb-4">
+            <div class="col-md-4">
+                <?php if(!empty($article->image)): ?>
+                    <img
+                        src="<?php echo htmlspecialchars($article->image); ?>"
+                        class="img-fluid"
+                        alt="Blog Post Image"
+                    >
+                <?php else: ?>
+                    <img
+                        src="https://dummyjson.com/image/350x200/333333/eae0d0"
+                        class="img-fluid"
+                        alt="Blog Post Image"
+                    >
+                <?php endif; ?>
+            </div>
+            <div class="col-md-8">
+                <h2><?php echo htmlspecialchars($article->title); ?></h2>
+                <p><?php echo htmlspecialchars($article->content); ?></p>
+                <a href="article.php?id=<?php echo $article->id; ?>" class="btn btn-primary">Read More</a>
+            </div>
         </div>
-        <div class="col-md-8">
-            <h2>Blog Post Title 1</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
-                pulvinar facilisis justo mollis, auctor consequat urna.
-            </p>
-            <a href="article.html" class="btn btn-primary">Read More</a>
-        </div>
-    </div>
 
-    <!-- Blog Post 2 -->
-    <div class="row mb-4">
-        <div class="col-md-4">
-            <img
-                src="https://dummyjson.com/image/350x200/333333/eae0d0"
-                class="img-fluid"
-                alt="Blog Post Image"
-            >
-        </div>
-        <div class="col-md-8">
-            <h2>Blog Post Title 2</h2>
-            <p>
-                Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, 
-                pharetra a, ultricies in, diam. Sed arcu.
-            </p>
-            <a href="#" class="btn btn-primary">Read More</a>
-        </div>
-    </div>
-
-    <!-- Blog Post 3 -->
-    <div class="row mb-4">
-        <div class="col-md-4">
-            <img
-                src="https://dummyjson.com/image/350x200/333333/eae0d0"
-                class="img-fluid"
-                alt="Blog Post Image"
-            >
-        </div>
-        <div class="col-md-8">
-            <h2>Blog Post Title 3</h2>
-            <p>
-                Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu 
-                vulputate magna eros eu erat.
-            </p>
-            <a href="#" class="btn btn-primary">Read More</a>
-        </div>
-    </div>
-
+        <?php endforeach; ?>
+    <?php endif; ?>
 </main>
 
 
