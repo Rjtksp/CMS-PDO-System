bc8bfbd15e0f738367e26ce891fb19c4d0f43847
Rajat Kashyap <35030513+Rjtksp@users.noreply.github.com>
2025-06-23 18:20:05 +0530

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -10,6 +10,7 @@ include "init.php";
     <title>CMS PDO System</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
